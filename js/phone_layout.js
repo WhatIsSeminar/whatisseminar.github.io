3dac672726422ac7baf67d796cf2284a72b54cdf
Mike Grunweg <grunweg@web.de>
2020-10-16 12:53:04 +0200

    Try to out-of-line the phone layout.

---
+++ js/phone_layout.js
@@ -0,0 +1,12 @@
+if (screen.width <= 699) {
+  var x;
+  x = document.getElementsByClassName("navbar-brand");
+  x[0].innerHTML = "w?S";
+  x = document.getElementsByClassName("col-md-2");
+  var i;
+  for (i = 0; i < 4; i++) {
+    x[i].style="text-align:left;";
+  }
+  x = document.getElementsByClassName("display-3");
+  x[0].className="";
+}
