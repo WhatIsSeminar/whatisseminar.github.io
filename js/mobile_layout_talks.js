85de875ad8355cbd22215598b3bb1f492f49bb57
tonellicueto <josue.tonelli.cueto@gmail.com>
2020-12-22 17:08:34 +0100

    Mobile display
    Making the display modification dynamic according to the number of titles

--- js/mobile_layout_talks.js
+++ js/mobile_layout_talks.js
@@ -4,7 +4,7 @@ if (screen.width <= 699) {
   x[0].innerHTML = "w?S";
   x = document.getElementsByClassName("col-md-2");
   var i;
-  for (i = 0; i < 4; i++) {
+  for (i = 0; i < x.length; i++) {
     x[i].style="text-align:left;";
   }
   x = document.getElementsByClassName("display-3");
